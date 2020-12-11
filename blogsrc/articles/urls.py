58ca3a7dd1f3bd9eab2fbfabc3408875d66ac3f1
Mesut Çifci <mesutcifci97@gmail.com>
2020-12-12 00:56:05 +0300

    personal-blog front-end updated
    - detail page added (design not complete)
    - html, css file structure modified

--- blogsrc/articles/urls.py
+++ blogsrc/articles/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ArticleDetailView, ArticleListView
 
 urlpatterns = [
-    path('', ArticleListView.as_view(), name='article_list'),
+    path('', ArticleListView.as_view(), name='index'),
     path('<slug:slug>/', ArticleDetailView.as_view(), name='article_detail')
     
 ]
