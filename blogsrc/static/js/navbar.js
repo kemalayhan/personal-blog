9d913629bf0a5ffdf51c5bf04a60c3540da8a1ea
Mesut Çifci <mesutcifci97@gmail.com>
2020-12-13 20:43:14 +0300

    -front-end updated
    -responsive navbar design almost finished

--- blogsrc/static/js/navbar.js
+++ blogsrc/static/js/navbar.js
@@ -1,5 +1,16 @@
+let navbar = document.querySelector(".navbar")
 let hamburgerMenu = document.querySelector(".hamburger-menu");
 
 hamburgerMenu.addEventListener("click", () => {
    hamburgerMenu.classList.toggle("js-rotate");
+   for(let element of hamburgerMenu.children) {
+      element.classList.toggle("js-color");
+   }
+   if(navbar.classList.contains("js-hide")) {
+      navbar.classList.remove("js-hide");
+   }else {
+      navbar.classList.add("js-hide")
+   }
 });
+
+
