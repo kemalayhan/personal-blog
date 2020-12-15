fd569e2490b4f5d415dadfa13df928d673eaacca
Kemal Ayhan <kemalayhan013@gmail.com>
2020-12-15 13:39:14 +0300

    Added template view for about page

--- blogsrc/blogsrc/urls.py
+++ blogsrc/blogsrc/urls.py
@@ -1,13 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+from django.views.generic import TemplateView
 from django.contrib import admin
 from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('articles.urls'), name='articles'),
+    path('about', TemplateView.as_view(template_name="about.html"), name="about"),
     re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
